84b9e09bb6406eaffa63365a87bf59a997f46f5f
jsahm <contact@johnstyle.fr>
2013-01-14 07:55:28 +0100

    Update bashrc alias

--- bashrc/alias.sh
+++ bashrc/alias.sh
@@ -3,8 +3,3 @@
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-alias ssh-portable='ssh john@192.168.1.63 -p 6060 -i ~/.ssh/id_dsa'
-alias ssh-kimsufi='ssh jsahm@91.121.174.99 -p 6060 -i ~/.ssh/id_dsa'
-alias ssh-nas='ssh admin@192.168.1.83 -p 6060 -i ~/.ssh/id_dsa'
-alias ssh-droid='ssh root@192.168.1.51 -p 2222 -i ~/.ssh/id_dsa'
-alias ssh-xbmc='ssh root@192.168.1.35 -i ~/.ssh/id_dsa'

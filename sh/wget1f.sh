8886d3ad7555031102774fc5c38e03915d5c170a
Jonathan SAHM <contact@johnstyle.fr>
2013-05-17 08:18:26 +0200

    Mep scripts wget

---
+++ sh/wget1f.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+ABSPATH=$(dirname $(readlink -f $0))
+
+if [ -f "${ABSPATH}/wget1f.conf" ]; then
+    . ${ABSPATH}/wget1f.conf
+fi
+
+url=''
+options=''
+
+USER_AGENT="Mozilla/5.0 (Linux; U; Linux x86; fr-FR; rv:1.7.5) Gecko/20041202 Firefox/1.0"
+
+cd ~/Téléchargements/
+
+if [ "${1}" ]; then
+    url="${1}"
+elif [ -f "wget1f.txt" ]; then
+    url="wget1f.txt"
+    options=" -i "
+fi
+
+if [ "${url}" ]; then
+
+    if [ "${login}" && "${password}" ]; then
+
+        wget --quiet \
+             --post-data "mail={$login}&pass={$password}&lt=on&valider=Envoyer" \
+             --save-cookies wget1f.cookies \
+             --keep-session-cookies \
+             --user-agent "${USER_AGENT}" \
+             -O /dev/null \
+             "http://www.1fichier.com/login.pl"
+
+        wget --load-cookies wget1f.cookies \
+             --user-agent "${USER_AGENT}" \
+             --content-disposition \
+             -c \
+             ${options} \
+             "${url}"
+
+        rm wget1f.cookies
+        
+    else
+
+        wget --user-agent "${USER_AGENT}" \
+             --content-disposition \
+             -c \
+             ${options} \
+             "${url}"
+
+    fi
+else 
+    echo "Veuillez renseigner un ou des URL à télécharger." 
+fi
+
+if [ -f "wget1f.txt" ]; then
+    rm wget1f.txt
+fi

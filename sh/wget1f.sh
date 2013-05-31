9ad4180ecd608ff368320d8d0d0b1163174b9cef
Jonathan SAHM <contact@johnstyle.fr>
2013-05-31 21:59:32 +0200

    Maj scripts wget

--- sh/wget1f.sh
+++ sh/wget1f.sh
@@ -1,51 +1,118 @@
 #!/bin/bash
 
 ABSPATH=$(dirname $(readlink -f $0))
+DEFAULT_LIST_FILE="wget1f.txt"
 
-if [ -f "${ABSPATH}/wget1f.conf" ]; then
-    . ${ABSPATH}/wget1f.conf
-fi
-
-url=''
-options=''
-
-USER_AGENT="Mozilla/5.0 (Linux; U; Linux x86; fr-FR; rv:1.7.5) Gecko/20041202 Firefox/1.0"
-
-cd ~/Téléchargements/
+success=false
 
 if [ "${1}" ]; then
-    url="${1}"
-elif [ -f "wget1f.txt" ]; then
-    url="wget1f.txt"
-    options=" -i "
+    LIST_FILE=${1}
+else
+    LIST_FILE=${DEFAULT_LIST_FILE}
 fi
 
-if [ "${url}" ]; then
-
-    if [[ "${login}" && "${password}" ]]; then
-
+function getCookie {
+    if [ ! -f "${SCRIPT_PATH}/wget1f.cookies" ]; then
         wget --quiet \
-             --post-data "mail=${login}&pass=${password}&lt=on&valider=Envoyer" \
-             --save-cookies wget1f.cookies \
+             --post-data "mail=${LOGIN}&pass=${PASSWORD}&lt=on&valider=Envoyer" \
+             --save-cookies "${SCRIPT_PATH}/wget1f.cookies" \
              --keep-session-cookies \
              --user-agent "${USER_AGENT}" \
              -O /dev/null \
              "http://www.1fichier.com/login.pl"
-
-        wget --load-cookies wget1f.cookies \
-             --user-agent "${USER_AGENT}" \
-             --content-disposition \
-             -c \
-             ${options} \
-             "${url}"
-
-        rm wget1f.cookies
-
     fi
-else 
-    echo "Veuillez renseigner un ou des URL à télécharger." 
+}
+
+function download {
+    response=$(wget --load-cookies "${SCRIPT_PATH}/wget1f.cookies" \
+         --user-agent "${USER_AGENT}" \
+         --content-disposition \
+         --server-response \
+         -c \
+         "${1}" 2>&1 | grep "Enregistre :")
+    filename=$(echo ${response} | sed -e 's/^.*«\(.*\)»$/\1/')
+    log="$(date '+%Y-%m-%d %r') | ${1} | ${filename}"
+    echo "${log}" >> "${SCRIPT_PATH}/${LIST_FILE}.log"
+    echo "${log}"
+    if [ -f "${DOWNLOAD_PATH}/${filename}" ]; then      
+        success=true
+    elif [ ! $(grep "Attention" "${DOWNLOAD_PATH}/index.html") ]; then
+        success=true
+    else
+        success=false
+    fi    
+}
+
+function removeLine {
+    if [ -f "${LIST_FILE}" ]; then
+        if [ ! -f "${SCRIPT_PATH}/${LIST_FILE}.lock" ]; then
+            touch "${SCRIPT_PATH}/${LIST_FILE}.lock"
+            sed "s~${1}~~g" ${LIST_FILE} > "${SCRIPT_PATH}/${LIST_FILE}.tmp"
+            mv "${SCRIPT_PATH}/${LIST_FILE}.tmp" ${LIST_FILE}
+            rm "${SCRIPT_PATH}/${LIST_FILE}.lock"
+        else
+            sleep 1
+            removeLine ${1}
+        fi
+    fi
+}
+
+if [ -f "${ABSPATH}/wget1f.conf" ]; then
+
+    . ${ABSPATH}/wget1f.conf
+
+    if [ ! "${USER_AGENT}" ]; then
+        USER_AGENT="Mozilla/5.0 (Linux; U; Linux x86; fr-FR; rv:1.7.5) Gecko/20041202 Firefox/1.0"
+    fi
+
+    if [ ! "${DOWNLOAD_PATH}" ]; then
+        DOWNLOAD_PATH="${HOME}/Téléchargements"
+    fi
+    
+    if [ ! -d "${DOWNLOAD_PATH}" ]; then
+        mkdir ${DOWNLOAD_PATH}
+    fi
+    
+    if [ -d "${DOWNLOAD_PATH}" ]; then
+    
+        cd ${DOWNLOAD_PATH}
+        
+        SCRIPT_PATH="${DOWNLOAD_PATH}/.wget1f"
+        
+        if [ ! -d "${SCRIPT_PATH}" ]; then
+            mkdir ${SCRIPT_PATH}
+        fi
+
+        urls=()
+
+        if [ -f "${LIST_FILE}" ]; then
+            for line in $(cat ${LIST_FILE}); do
+                if [ "${line}" ]; then
+                    urls+=("${line}")
+                fi
+            done            
+        fi
+
+        if [ "${urls}" ]; then
+
+            cp ${LIST_FILE} "${SCRIPT_PATH}/${LIST_FILE}.bk"
+
+            getCookie
+
+            for url in ${urls[@]}; do
+                download ${url}
+                if [[ "${success}" = true ]]; then
+                    removeLine ${url}
+                fi
+            done
+        else
+            echo "Veuillez renseigner une URL"
+        fi      
+    else
+        echo "Impossible d'acceder au dossier ${DOWNLOAD_PATH}"  
+    fi
+else
+    echo "LOGIN=''\nPASSWORD=''\nDOWNLOAD_PATH=''\nUSER_AGENT=''" > "${ABSPATH}/wget1f.conf"
+    echo "Veuillez complèter le fichier ${ABSPATH}/wget1f.conf"
 fi
 
-if [ -f "wget1f.txt" ]; then
-    rm wget1f.txt
-fi
